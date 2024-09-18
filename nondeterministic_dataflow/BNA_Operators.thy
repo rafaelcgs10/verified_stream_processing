4b2370337dc61ce246d13737759d11a69bd7e7f7
Asta Halkjær From <andro.from@gmail.com>
2024-09-18 15:34:17 +0200

    more

--- nondeterministic_dataflow/BNA_Operators.thy
+++ nondeterministic_dataflow/BNA_Operators.thy
@@ -36,7 +36,7 @@ abbreviation sum_assoc :: \<open>('a + 'b) + 'c \<Rightarrow> 'a + ('b + 'c)\<cl
   \<open>sum_assoc \<equiv> case_sum (case_sum Inl (Inr o Inl)) (Inr o Inr)\<close>
 
 lemma
-  assumes \<open>history (bna_parcomp a (bna_parcomp b c)) lin lout\<close> \<open>history (bna_parcomp (bna_parcomp a b) c) rin rout\<close>
+  assumes \<open>history (bna_parcomp a (bna_parcomp b c)) lin lout\<close> \<open>history (bna_parcomp (bna_parcomp a b) c) (lin o sum_assoc) rout\<close>
   shows \<open>lout (sum_assoc p) = rout p\<close>
   using assms unfolding history_def traced_pcomp_op'
   apply auto
