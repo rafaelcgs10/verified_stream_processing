1193c9fa91729bc0eac8b400e9fe6a3b7e4e41f8
Dmitriy Traytel <traytel@di.ku.dk>
2025-01-29 18:20:38 +0100

    code

--- nondeterministic_dataflow/Eval.thy
+++ nondeterministic_dataflow/Eval.thy
@@ -14,11 +14,11 @@ definition W42 :: "(2,1,nat) op" where "W42 = Write end_op 1 42"
 definition CP :: "(1,1,bool) op" where "CP = Read 1 (\<lambda>x. Write end_op 1 x)"
 corec cp_op :: "(1,1,bool) op" where "cp_op = Read 1 (\<lambda>x. Write cp_op 1 x)"
 
-value "force_cset 10 (cimage fst (eval 10 W42))"
-value "force_cset 10 (cimage fst (eval 10 CP))"
-value "force_cset 100 (cimage fst (eval 10 cp_op))"
-value "force_cset 100 (cimage fst (eval 1000 (CP \<bullet> CP)))"
-value "force_cset 100 (cimage fst (eval 10 (cp_op \<bullet> cp_op)))"
-value "force_cset 100 (cimage fst (eval 10 (cp_op \<parallel> cp_op)))"
+value [GHC] "force_cset undefined 10 (cimage fst (eval 10 W42))"
+value [GHC] "force_cset undefined 10 (cimage fst (eval 10 CP))"
+value [GHC] "force_cset undefined 100 (cimage fst (eval 10 cp_op))"
+value [GHC] "force_cset undefined 100 (cimage fst (eval 1000 (CP \<bullet> CP)))"
+value [GHC] "force_cset undefined 100 (cimage fst (eval 10 (cp_op \<bullet> cp_op)))"
+value [GHC] "force_cset undefined 100 (cimage fst (eval 10 (cp_op \<parallel> cp_op)))"
 
 end
