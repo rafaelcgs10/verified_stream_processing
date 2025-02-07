ff45fb0971d7c49d351df50ad5185a8bb5d72f57
rafaelcgs10 <rafaelcgs10@gmail.com>
2025-02-07 16:57:46 +0100

    WIP: proving lifted R1

--- nondeterministic_dataflow/BNA_Axioms.thy
+++ nondeterministic_dataflow/BNA_Axioms.thy
@@ -2578,27 +2578,52 @@ lift_definition
   ('ip, 'op, 'd) operator \<Rightarrow> ('ip :: defaults, 'op :: defaults, 'd) operator" is loop_op
   by (smt (verit, del_insts) Diff_Diff_Int Diff_Int_distrib Int_Diff diff_shunt inputs_loop_op_le le_iff_inf outputs_loop_op_le)
 
-term map_op
+term "map_op projl projr"
 
 lift_definition
- map_operator :: "('a :: defaults \<Rightarrow> 'b :: defaults) \<Rightarrow> ('c :: defaults \<Rightarrow> 'd :: defaults) \<Rightarrow> ('a, 'c, 'e) operator \<Rightarrow> ('b, 'd, 'e) operator" is map_op
-  apply (auto simp add: op.set_map)
-  subgoal
-   
-
-
-  thm image_iff
-
-  find_theorems outputs map_op
-
-end
-
+ map_operator :: "('a :: defaults \<Rightarrow> 'b :: defaults) \<Rightarrow> ('c :: defaults \<Rightarrow> 'd :: defaults) \<Rightarrow> ('a, 'c, 'e) operator \<Rightarrow> ('b, 'd, 'e) operator" is 
+"\<lambda> f g op. (if f ` inputs op \<inter> defaults = {} \<and> g ` outputs op \<inter> defaults = {} then map_op f g op else end_op)"
+  by (auto simp add: op.set_map)
 
 no_notation scomp_op (infixl "\<bullet>" 65)
 definition scomp_operator (infixl "\<bullet>" 65) where
-  "scomp_operator op1 op2 = map_op projl projr (comp_operator Some (\<lambda>_. []) op1 op2)"
+  "scomp_operator op1 op2 = map_operator projl projr (comp_operator Some (\<lambda>_. []) op1 op2)"
+
+no_notation feedback_op ( "_ \<up>" [66] 65)
+no_notation pcomp_op (infixl "\<parallel>" 64)
+
+definition pcomp_operator (infixl "\<parallel>" 64) where
+  "pcomp_operator = comp_operator (\<lambda>_. None) (\<lambda>_. [])"
+
+definition feedback_operator ( "_ \<up>" [66] 65) where
+  "feedback_operator op = map_operator projl projl (loop_operator (case_sum (\<lambda> _. None) (\<lambda> p. if p \<in> defaults then None else (Some (Inr p)))) (case_sum undefined (\<lambda> _. [])) op)"
+
+lemma inputs_id_op[simp]:
+  "inputs (id_op buf) = UNIV - defaults"
+  sorry
+
+lemma outputs_id_op[simp]:
+  "outputs (id_op buf) = UNIV - defaults"
+  sorry
+
+lift_definition id_operator :: "('a \<Rightarrow> 'b buf) \<Rightarrow> ('a :: {countable, defaults}, 'a, 'b) operator" is id_op
+  by auto
+
+no_notation id_empty_op ("\<I>")
+
+abbreviation id_empty_operator ("\<I>") where
+  "\<I> \<equiv> id_operator (\<lambda> _. [])"
 
 
+lift_definition wbisim_operator :: "('a :: defaults, 'b :: defaults, 'c) operator \<Rightarrow> ('a, 'b, 'c) operator \<Rightarrow> bool" is wbisim.
+
+lemma loop_op_scomp_commute:
+  "wbisim_operator (op2 \<bullet> (op1\<up>)) (((op2 \<parallel> \<I>) \<bullet> op1)\<up>)"
+  unfolding pcomp_operator_def scomp_operator_def feedback_op_def
+  apply transfer
+  using loop_op_scomp_commute [unfolded scomp_op_def feedback_op_def pcomp_op_def]
+  inputs_comp_op_le[unfolded subset_eq, simplified] outputs_comp_op_le[unfolded subset_eq, simplified]
+  
 
 end
 
