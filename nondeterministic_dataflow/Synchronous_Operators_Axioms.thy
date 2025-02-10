3bb23c51f5d41243c5de717d645bca250e407040
Laouen Fernet <lafe@di.ku.dk>
2025-02-10 11:08:08 +0100

    WIP A11

--- nondeterministic_dataflow/Synchronous_Operators_Axioms.thy
+++ nondeterministic_dataflow/Synchronous_Operators_Axioms.thy
@@ -73,9 +73,37 @@ lemma acopy_op_aeq_op_id_op_bufs:
     unfolding wsim_def
     apply auto
     subgoal
-      sorry
+      apply (drule step_map_op_inv)
+      apply auto
+      apply (drule step_comp_op_cases)
+      apply auto
+      subgoal for p x
+        apply (erule step_acopy_op_Inp)
+         apply simp
+        apply (rule exI[of _ \<open>id_op (BENQ p x buf)\<close>])
+        apply (rule conjI)
+        apply fast
+        sorry
+          apply (meson no_step_aeq_op_Out no_step_acopy_op_Out)+
+        apply (simp split: sum.splits)
+      subgoal
+        apply (erule step_aeq_op_Inp_L)
+         apply simp
+        apply (rule exI[of _ \<open>id_op buf\<close>])
+        apply (rule conjI)
+         apply fast
+        sorry
+      subgoal
+        apply (erule step_aeq_op_Inp_R)
+         apply simp
+        apply (rule exI[of _ \<open>id_op buf\<close>])
+        apply (rule conjI)
+         apply fast
+        sorry
+       apply (meson no_step_acopy_op_Tau no_step_aeq_op_Tau)+
+      done
     subgoal
-      apply (drule step_id_op_cases)
+      apply (erule step_id_op_cases)
         apply auto
       subgoal for p x
         apply (rule exI[of _ \<open>map_op projl projr (comp_op Some (case_sum (BENQ p x buf) (BENQ p x buf)) \<C> \<Q>)\<close>])
@@ -92,6 +120,124 @@ lemma acopy_op_aeq_op_id_op_bufs:
       done
     done
   oops
+
+lemma acopy_op_aeq_op_id_op_bufs':
+  \<open>map_op projl projr (comp_op Some (case_sum buf buf) \<C> \<Q>) \<approx> id_op buf\<close>
+  apply (rule wbisim_coinduct_upto[of \<open>\<lambda>op1 op2.
+  (\<exists>buf. op1 = map_op projl projr (comp_op Some (case_sum buf buf) \<C> \<Q>) \<and> op2 = id_op buf)
+  \<or> (\<exists>buf p x. op1 = map_op projl projr (comp_op Some (case_sum buf buf) (Choice {|Write (Write \<C> (Inr p) x) (Inl p) x, Write (Write \<C> (Inl p) x) (Inr p) x|}) \<Q>) \<and> op2 = id_op (BENQ p x buf))
+  \<or> (\<exists>buf p. buf p \<noteq> [] \<and> op1 = map_op projl projr (comp_op Some (case_sum (BTL p buf) buf) \<C> (Read (Inr p) (\<lambda>x. if x = BHD p buf then Write \<Q> p x else Silent \<Q>))) \<and> op2 = id_op buf)
+  \<or> (\<exists>buf p. buf p \<noteq> [] \<and> op1 = map_op projl projr (comp_op Some (case_sum buf (BTL p buf)) \<C> (Read (Inl p) (\<lambda>x. if x = BHD p buf then Write \<Q> p x else Silent \<Q>))) \<and> op2 = id_op buf)
+  \<or> (\<exists>buf p x. op1 = map_op projl projr (comp_op Some (case_sum (BENQ p x buf) buf) (Write \<C> (Inr p) x) \<Q>) \<and> op2 = id_op (BENQ p x buf))
+  \<or> (\<exists>buf p x. op1 = map_op projl projr (comp_op Some (case_sum buf (BENQ p x buf)) (Write \<C> (Inl p) x) \<Q>) \<and> op2 = id_op (BENQ p x buf))\<close>])
+  unfolding wsim_def
+  apply auto
+  subgoal for buf
+    apply (drule step_map_op_inv)
+    apply auto
+    apply (drule step_comp_op_cases)
+    apply auto
+    subgoal for p x
+      apply (erule step_acopy_op_Inp)
+       apply simp
+      apply (rule exI[of _ \<open>id_op (BENQ p x buf)\<close>])
+      apply (rule conjI)
+       apply blast
+      apply (rule wbc_base)
+      apply fast
+      done
+        apply (meson no_step_aeq_op_Out no_step_acopy_op_Out)+
+      apply (simp split: sum.splits)
+    subgoal
+      apply (erule step_aeq_op_Inp_L)
+       apply simp
+      apply (rule exI[of _ \<open>id_op buf\<close>])
+      apply (rule conjI)
+       apply blast
+      apply (rule wbc_base)
+      apply fast
+      done
+    subgoal
+      apply (erule step_aeq_op_Inp_R)
+       apply blast+
+      done
+     apply (meson no_step_acopy_op_Tau no_step_aeq_op_Tau)+
+    done
+  subgoal for buf
+    apply (erule step_id_op_cases)
+      apply auto
+    subgoal for p x
+      apply (rule exI[of _ \<open>map_op projl projr (comp_op Some (case_sum (BENQ p x buf) (BENQ p x buf)) \<C> \<Q>)\<close>])
+      apply (rule conjI[rotated])
+       apply (rule wbc_sym)
+       apply auto
+      apply (rule step_io_step_tau_tau_wstep)
+        apply fastforce+
+      done
+    subgoal for p
+      apply (rule exI[of _ \<open>map_op projl projr (comp_op Some (case_sum (BTL p buf) (BTL p buf)) \<C> \<Q>)\<close>])
+      apply (rule conjI[rotated])
+       apply (rule wbc_sym)
+       apply auto
+      apply (rule step_tau_step_tau_step_io_wstep)
+        apply fastforce+
+      done
+    done
+  subgoal for buf p x
+    apply (drule step_map_op_inv)
+    apply auto
+    apply (drule step_comp_op_cases)
+    apply auto
+        apply (meson no_step_aeq_op_Out)
+       apply blast
+      apply blast
+    apply (simp split: sum.splits)
+  oops
+
+lemma
+  \<open>buf p \<noteq> [] \<Longrightarrow>
+  comp_op Some (case_sum (BTL p buf) buf) \<C> (Read (Inr p) (\<lambda>x. if x = BHD p buf then Write \<Q> p x else Silent \<Q>))
+  \<greatersim> comp_op Some (case_sum buf buf) \<C> \<Q>\<close>
+  apply (coinduction arbitrary: buf p rule: expand.coinduct)
+  unfolding wsim_def expansion_def
+  subgoal for buf p
+    apply auto
+    subgoal
+      apply (drule step_comp_op_cases)
+      apply auto
+      subgoal for p' x
+        apply (erule step_acopy_op_Inp)
+         apply simp
+        apply (rule exI[of _ \<open>comp_op Some (case_sum (BTL p buf) buf) (Choice {|Write (Write \<C> (Inr p') x) (Inl p') x, Write (Write \<C> (Inl p') x) (Inr p') x|}) (Read (Inr p) (\<lambda>x. if x = BHD p buf then Write \<Q> p x else Silent \<Q>))\<close>])
+        apply (rule conjI)
+         apply (rule step_io_step_tau_wstep)
+          apply fast+
+        apply (rule disjI2)
+        apply (simp add: expand_refl)
+        done
+        apply (meson no_step_acopy_op_Out)
+      subgoal
+        apply (rule exI[of _ \<open>comp_op Some (case_sum (BTL p buf) (BTL p buf)) \<C> (Write \<Q> p (BHD p buf))\<close>])
+        apply (rule conjI)
+         apply (rule rtranclp.intros(2))
+        apply (rule rtranclp.intros(2))
+           apply (rule rtranclp.intros(1))
+          apply blast
+        using step_tau_comp_op_Inl_case_sum[of p \<open>BHD p buf\<close> \<open>Read (Inl p) (\<lambda>x. if x = BHD p buf then Write \<Q> p x else Silent \<Q>)\<close> \<open>Write \<Q> p (BHD p buf)\<close> buf \<open>BTL p buf\<close> \<C>]
+         apply (smt (verit, ccfv_SIG) SR)
+        apply (rule disjI2)
+        apply (rule expand_refl)
+        done
+      apply (meson no_step_acopy_op_Tau)
+      done
+    subgoal
+      apply (drule step_comp_op_cases)
+      apply auto
+      subgoal for p' x
+        apply (erule step_acopy_op_Inp)
+        apply simp
+  oops
+
 
 lemma acopy_op_aeq_op_id_op_bufs:
   \<open>map_op projl projr (comp_op Some (case_sum buf buf) \<C> \<Q>) \<approx> id_op buf\<close>
