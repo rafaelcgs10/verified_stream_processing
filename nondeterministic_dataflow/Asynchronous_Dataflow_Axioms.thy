f8ba41399afcadca95db23a27ae2e9bc1ff4e0c1
rafaelcgs10 <rafaelcgs10@gmail.com>
2025-03-05 12:42:00 +0100

    Fixes non terminating

--- nondeterministic_dataflow/Asynchronous_Dataflow_Axioms.thy
+++ nondeterministic_dataflow/Asynchronous_Dataflow_Axioms.thy
@@ -1881,7 +1881,12 @@ proof (coinduction arbitrary: buf1 buf1' buf2 rule: wbisim_coinduct_upto)
       and op1' :: "('a, 'b, 'c) op"
     assume H: "step io sink_op op1'"
     show "\<exists>op2'. wstep io (map_op projl projl (loop_op (case_sum (\<lambda>_. None) (\<lambda>p. if p \<in> defaults then None else Some (Inr p))) (case_sum undefined buf2) (map_op id Inr (merge_op (case_sum buf1 buf1'))))) op2' \<and> wbisim_cong (\<lambda>op1 op2. \<exists>buf1 buf1' buf2. op1 = map_op projl projl (loop_op (case_sum (\<lambda>_. None) (\<lambda>p. if p \<in> defaults then None else Some (Inr p))) (case_sum undefined buf2) (map_op id Inr (merge_op (case_sum buf1 buf1')))) \<and> op2 = sink_op) op1' op2'"
-      using H by (elim step_sink_op) force
+      using H apply (elim step_sink_op) 
+      apply (intro exI conjI[rotated])
+       apply (rule wbc_sym)
+       apply (rule wbc_base)
+       apply fastforce+
+      done
   qed
 qed
 
