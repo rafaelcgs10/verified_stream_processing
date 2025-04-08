edd4a4618b8413166c45b0a45f8f82facbbab144
rafaelcgs10 <rafaelcgs10@gmail.com>
2025-04-08 16:08:10 +0200

    filter example

--- nondeterministic_dataflow/Operator_Examples.thy
+++ nondeterministic_dataflow/Operator_Examples.thy
@@ -4,45 +4,45 @@ imports
   Operator
 begin
 
-
 corec writes where
   "writes op p xs =
     (case xs of [] \<Rightarrow> case_op Read Write Choice Silent op | x #xs \<Rightarrow> Write (writes op p xs) p x)"
 
+lemma foo[friend_of_corec_simps]:
+  "(if snd (snd x) = [] then ctor_op (Abs_op_pre_op (Rep_op_pre_op (dtor_op (fst x)))) else ctor_op (Abs_op_pre_op (Inl (Inr (algrho (fst x, fst (snd x), btl (snd (snd x))), fst (snd x), bhd (snd (snd x))))))) =
+         (if snd (snd x) = []
+         then if isl (Rep_op_pre_op (dtor_op (fst x))) \<and> isl (projl (Rep_op_pre_op (dtor_op (fst x)))) then ctor_op (Abs_op_pre_op (Rep_op_pre_op (dtor_op (fst x))))
+              else if isl (Rep_op_pre_op (dtor_op (fst x))) \<and> \<not> isl (projl (Rep_op_pre_op (dtor_op (fst x)))) then ctor_op (Abs_op_pre_op (Rep_op_pre_op (dtor_op (fst x))))
+                   else if \<not> isl (Rep_op_pre_op (dtor_op (fst x))) \<and> isl (projr (Rep_op_pre_op (dtor_op (fst x)))) then ctor_op (Abs_op_pre_op (Rep_op_pre_op (dtor_op (fst x))))
+                        else ctor_op
+                              (Abs_op_pre_op
+                                (Inr (Inr (if isl (Rep_op_pre_op (dtor_op (fst x))) then undefined
+                                           else if isl (projr (Rep_op_pre_op (dtor_op (fst x)))) then undefined else projr (projr (Rep_op_pre_op (dtor_op (fst x))))))))
+         else ctor_op (Abs_op_pre_op (Inl (Inr (algrho (fst x, fst (snd x), btl (snd (snd x))), fst (snd x), bhd (snd (snd x)))))))"
+  by (auto split: if_splits)
+
 friend_of_corec writes where
   "writes op p xs =
     (case xs of [] \<Rightarrow> case_op Read Write Choice Silent op | x #xs \<Rightarrow> Write (writes op p xs) p x)"
-  sorry
-
-(* 
-consts window_op :: "'a buf \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> (1, 1, 'd) op"  *)
-
+  apply (rule writes.code)
+  apply transfer_prover
+  done
 
 corec window_op where
- "window_op f buf timer time_win =
+ "window_op f n buf time =
    Choice (cimage (\<lambda> time. 
      choice2
-     (Read (1::1) (\<lambda> x. if time_win < time then writes (window_op f [] (time mod time_win) time_win) 1 (f (buf @ [x]) # replicate 0 (f [])) else Silent (window_op f (buf @ [x]) time time_win)))
-     (if buf = [] then Silent (window_op f buf 0 time_win) else (if time_win < time then Write (window_op f [] (time mod time_win) time_win) 1 (f buf) else Silent (window_op f buf time time_win)))
-     ) (cset.acset {Suc timer..}))"
+     (Read (1::1) (\<lambda> x. if n < time then writes (window_op f n [] (time mod n)) (1 :: 1) (f (buf @ [x]) # replicate (time div n - 1) (f [])) else Silent (window_op f n (buf @ [x]) time)))
+     (if n < time then writes (window_op f n [] (time mod n)) 1 (f buf # replicate (time div n - 1) (f [])) else Silent (window_op f n buf time))
+     ) (cset.acset {Suc time..}))"
 
-lemma window_op_code:
-  "window_op buf timer time_win =
-   Choice (cimage (\<lambda> time. 
-     Choice {|
-     Read (1::1) (\<lambda> x. if time_win < time then Write (window_op [] 0 time_win) 1 (encode_output (buf @ [decode_input x])) else Silent (window_op (buf @ [decode_input x]) time time_win)),
-     if buf = [] then Silent (window_op buf 0 time_win) else (if time_win < time then Write (window_op [] 0 time_win) 1 (encode_output buf) else Silent (window_op buf time time_win))
-     |}) (cset.acset {Suc timer..}))"
-  sorry
+corec filter_op where
+  "filter_op P buf = choice2 
+   (Read (1 :: 1) (\<lambda> x. filter_op P (if P x then buf @ [x] else buf)))
+   (writes (filter_op P []) 1 buf)"
 
+term wtraced
 
-end
-
-global_interpretation wop: window Inl projl Inr projr
-  defines wwindow_op = "wop.window_op"
-  by standard auto
-
-term wwindow_op
-
+thm wbisim'_alt
 
 end
