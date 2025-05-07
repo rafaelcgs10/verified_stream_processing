4d2db0052108f922155078b6868bd40125222776
rafaelcgs10 <rafaelcgs10@gmail.com>
2025-05-07 16:01:48 +0200

    WIP

--- nondeterministic_dataflow/Timely_Operators.thy
+++ nondeterministic_dataflow/Timely_Operators.thy
@@ -5,20 +5,54 @@ imports
   BNA_Operators
 begin
 
-datatype 'd flow = 
-  Node "(nat, nat, 'd) op"
-  | Seq "'d flow" "'d flow"
-  | Par "'d flow" "'d flow"
-  | Loop "'d flow"
-
-term "Seq (Node ex1_op) (Node !)"
-
-term "Seq
-      (Node (map_op (case_sum ((*) 2) (\<lambda> x. 2 * x + 1)) id \<oslash>))
-      (Node (map_op id (case_sum ((*) 2) (\<lambda> x. 2 * x + 1)) \<Lambda>))"
+datatype ('c, 'd) subgraph = 
+  "apply": Logic "'c \<Rightarrow> (nat, nat, 'd) op"
+  | Seq "nat \<Rightarrow> 'd buf" "('c, 'd) subgraph" "('c, 'd) subgraph"
 
 
-term "()"
-term "\<lambda> x. if x = 1 then Some (Inl \<V>) else None"
+inductive activate where
+  "step io (l c) (l' c') \<Longrightarrow> activate io c (Logic l) c' (Logic l')"
+| "activate (Out p x) c op1 c' op1' \<Longrightarrow>
+   activate Tau c (Seq buf op1 op2) c' (Seq (BENQ p x buf) op1' op2)" 
+ 
+(* friend_of_corec map_op where
+  "map_op f g (op :: ('i, 'o, 'd) op) = 
+  (case op of Silent op' \<Rightarrow> 
+    Silent (map_op f g op') 
+  | Write op' p x \<Rightarrow> Write (map_op f g op') (g p) x 
+  | Read p f' \<Rightarrow> Read (f p) (map_op f g o f')
+  | Choice ops \<Rightarrow> Choice (map_op f g |`| ops))"
+ *)
+
+
+
+fun compile_subgraph where
+  "compile_subgraph c (Logic l) = l c"
+| "compile_subgraph c (Seq buf sg1 sg2) = map_op projl projr (comp_op Some buf (compile_subgraph c sg1) (compile_subgraph c sg2))"
+
+lemma
+  "activate io c t_op c' t_op' \<Longrightarrow>
+   step io (compile_subgraph c t_op) (compile_subgraph c' t_op')"
+  apply (induct io c t_op c' t_op' pred: activate)
+  subgoal for io op c op' c'
+    by simp
+  subgoal for p x c op1 c' op1' buf op2
+    apply simp
+      apply (rule step_map_op)
+     apply simp_all
+    apply (rule step_Tau_comp_op_L)
+    apply simp_all
+
+      find_theorems comp_op Tau Out
+
+
+
+
+
+
+
+
+
+
    
 end
