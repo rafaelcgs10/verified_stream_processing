ab9a4353b8e9d13eef6325cdaf3bd9c6bd1c7bd4
rafaelcgs10 <rafaelcgs10@gmail.com>
2025-05-09 14:40:39 +0200

    WIP

--- nondeterministic_dataflow/Timely_Operators.thy
+++ nondeterministic_dataflow/Timely_Operators.thy
@@ -3,17 +3,12 @@ theory Timely_Operators
 imports
   Operator
   BNA_Operators
+  Progress_Tracking.Propagate
 begin
 
 datatype ('c, 'd) subgraph = 
   "apply": Logic "'c \<Rightarrow> (nat, nat, 'd) op"
-  | Seq "nat \<Rightarrow> nat option" "nat \<Rightarrow> 'd buf" "('c, 'd) subgraph" "('c, 'd) subgraph"
-
-(* inductive activate where
-  "step io (l c) (l' c') \<Longrightarrow> activate io c (Logic l) c' (Logic l')"
-| "activate (Out p x) c sg1 c' sg1' \<Longrightarrow>
-   activate Tau c (Seq buf sg1 sg2) c' (Seq (BENQ p x buf) sg1' sg2)" 
- *)
+  | Comp "nat \<Rightarrow> nat option" "nat \<Rightarrow> 'd buf" "('c, 'd) subgraph" "('c, 'd) subgraph"
 
 fun embed_nat where
   "embed_nat (Inl n) =  2 * n"
@@ -21,37 +16,22 @@ fun embed_nat where
 
 fun compile_subgraph where
   "compile_subgraph c (Logic l) = l c"
-| "compile_subgraph c (Seq wire buf sg1 sg2) = map_op embed_nat embed_nat (comp_op wire buf (compile_subgraph c sg1) (compile_subgraph c sg2))"
+| "compile_subgraph c (Comp wire buf sg1 sg2) = map_op embed_nat embed_nat (comp_op wire buf (compile_subgraph c sg1) (compile_subgraph c sg2))"
 
-(* 
-lemma
-  "activate io c sg c' sg' \<Longrightarrow>
-   step io (compile_subgraph c sg) (compile_subgraph c' sg')"
-  apply (induct io c sg c' sg'  pred: activate)
-  subgoal for io l c l' c'
-    by simp
-  subgoal for p x c sg1 c' sg1' buf sg2
-    apply simp
-      apply (rule step_map_op)
-     apply simp_all
-    apply (rule step_Tau_comp_op_L)
-       apply simp_all
-    oops *)
-
-inductive wactivate where
-  "step io (l c) (l' c') \<Longrightarrow> wactivate io c (Logic l) c' (Logic l')"
-| "wactivate (Out p x) c sg1 c' sg1' \<Longrightarrow>
+inductive activate where
+  "wstep io (l c) (l' c') \<Longrightarrow> activate io c (Logic l) c' (Logic l')"
+| "activate (Out p x) c sg1 c' sg1' \<Longrightarrow>
    wire p = Some q \<Longrightarrow>
-   wactivate Tau c sg2 c' sg2 \<Longrightarrow>
-   wactivate Tau c (Seq wire buf sg1 sg2) c' (Seq wire (BENQ q x buf) sg1' sg2)"
- | "wactivate (Inp p x) c sg2 c' sg2' \<Longrightarrow>
+   activate Tau c sg2 c' sg2 \<Longrightarrow>
+   activate Tau c (Comp wire buf sg1 sg2) c' (Comp wire (BENQ q x buf) sg1' sg2)"
+ | "activate (Inp p x) c sg2 c' sg2' \<Longrightarrow>
    BHD p buf = x \<Longrightarrow> buf p \<noteq> [] \<Longrightarrow>
    p \<in> ran wire \<Longrightarrow>
-   wactivate Tau c sg1 c' sg1' \<Longrightarrow>
-   wactivate Tau c (Seq wire buf sg1 sg2) c' (Seq wire (BTL p buf) sg1' sg2')" 
- | "wactivate (Inp p x) c sg1 c' sg1' \<Longrightarrow>
-   wactivate Tau c sg2 c' sg2' \<Longrightarrow>
-   wactivate (Inp (2 * p) x) c (Seq wire buf sg1 sg2) c' (Seq wire buf sg1' sg2')"  
+   activate Tau c sg1 c' sg1' \<Longrightarrow>
+   activate Tau c (Comp wire buf sg1 sg2) c' (Comp wire (BTL p buf) sg1' sg2')" 
+ | "activate (Inp p x) c sg1 c' sg1' \<Longrightarrow>
+   activate Tau c sg2 c' sg2' \<Longrightarrow>
+   activate (Inp (2 * p) x) c (Comp wire buf sg1 sg2) c' (Comp wire buf sg1' sg2')"  
 
 lemma wstep_trans_taus:
   "wstep io op op' \<Longrightarrow> wstep Tau op' op'' \<Longrightarrow> wstep io op op''"
@@ -71,12 +51,11 @@ lemma wstep_Inp_comp_op:
   apply blast
   done
 
-lemma
-  "wactivate io c sg c' sg' \<Longrightarrow>
+lemma activate_compiles:
+  "activate io c sg c' sg' \<Longrightarrow>
    wstep io (compile_subgraph c sg) (compile_subgraph c' sg')"
-  apply (induct io c sg c' sg'  pred: wactivate)
+  apply (induct io c sg c' sg'  pred: activate)
   subgoal for io l c l' c'
-    apply (rule step_wstep)
     apply simp
     done
   subgoal for p x c sg1 c' sg1' sg2 buf
@@ -90,6 +69,39 @@ lemma
   subgoal for p x c sg1 c' sg1' sg2 sg2' buf
     by (simp add: wstep_Inp_comp_op)
   done
+
+record ('loc, 't) progress =
+  conf :: "('loc, 't) configuration"
+  cap :: "'loc \<Rightarrow> 't zmultiset"
+
+abbreviation "init_conf \<equiv> \<lparr>c_work = (\<lambda> _. {#}\<^sub>z), c_pts = (\<lambda> _. {#}\<^sub>z), c_imp = (\<lambda> _. {#}\<^sub>z)\<rparr>"
+abbreviation "init_prog \<equiv> \<lparr> conf = init_conf, cap = \<lambda> _. {# 1 #}\<^sub>z \<rparr>"
+
+corec op1 :: "(2, nat) progress \<Rightarrow> (nat, nat, nat) op" where
+  "op1 pg = (if zcount ((cap pg) 1) 1 > 0 then Write (op1 (pg\<lparr>cap := (cap pg)(1 := {#}\<^sub>z)\<rparr>)) 1 0  else Silent (op1 pg))"
+
+corec op2 :: "(2, nat) progress \<Rightarrow> (nat, nat, nat) op" where
+  "op2 pg = pull 1 (case_option (Silent (op2 pg)) (\<lambda> x. if \<exists> t. t \<in>\<^sub>A frontier ((c_imp (conf pg)) 2) \<and> 1 \<le> t then Write \<oslash> 1 x else Silent (op2 pg)))"
+
+abbreviation "op1_sg \<equiv> Logic op1"
+abbreviation "op2_sg \<equiv> Logic op2"
+abbreviation "op1_op2_sg \<equiv> Comp Some (\<lambda> _. []) op1_sg op2_sg"
+
+lemma activate_op1_sg:
+  "activate (Out 1 0) init_prog op1_sg (init_prog\<lparr>cap := (\<lambda> _. {# 1 #}\<^sub>z)(1 := {#}\<^sub>z)\<rparr>) op1_sg"
+  apply (rule activate.intros(1))
+  apply (subst op1.code)
+  apply auto
+  done
+
+lemma
+  "activate Tau init_prog op1_op2_sg (init_prog\<lparr>cap := (\<lambda> _. {# 1 #}\<^sub>z)(1 := {#}\<^sub>z)\<rparr>) (Comp Some (BENQ 1 0 (\<lambda> _. [])) op1_sg op2_sg)"
+  apply (rule activate.intros(2)[where p=1])
+    apply (rule activate_op1_sg)
+   apply simp
+  apply (rule activate.intros(1))
+  apply simp
+  apply (rule rtranclp_intros)
 
 
 
