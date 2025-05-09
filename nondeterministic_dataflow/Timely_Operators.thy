0b367e81c2d35008250624d508c33700d04208ac
rafaelcgs10 <rafaelcgs10@gmail.com>
2025-05-09 09:27:10 +0200

    WIP simple

--- nondeterministic_dataflow/Timely_Operators.thy
+++ nondeterministic_dataflow/Timely_Operators.thy
@@ -6,35 +6,31 @@ imports
 begin
 
 datatype ('c, 'd) subgraph = 
-  "apply": Logic "'c \<Rightarrow> 'c \<times> (nat, nat, 'd) op"
+  "apply": Logic "'c \<Rightarrow> (nat, nat, 'd) op"
   | Seq "nat \<Rightarrow> 'd buf" "('c, 'd) subgraph" "('c, 'd) subgraph"
 
 inductive activate where
-  "l c = (c', op) \<Longrightarrow> l' c' = (_, op') \<Longrightarrow> step io op op' \<Longrightarrow> activate io c (Logic l) c' (Logic l')"
-| "activate (Out p x) c op1 c' op1' \<Longrightarrow>
-   activate Tau c (Seq buf op1 op2) c' (Seq (BENQ p x buf) op1' op2)" 
+  "step io (l c) (l' c') \<Longrightarrow> activate io c (Logic l) c' (Logic l')"
+| "activate (Out p x) c sg1 c' sg1' \<Longrightarrow>
+   activate Tau c (Seq buf sg1 sg2) c' (Seq (BENQ p x buf) sg1' sg2)" 
 
 fun compile_subgraph where
   "compile_subgraph c (Logic l) = l c"
-| "compile_subgraph c (Seq buf sg1 sg2) = (
-  let (c', op1) = (compile_subgraph c sg1) in 
-  let (c'', op2) = (compile_subgraph c' sg2) in 
-  (c'', map_op projl projr (comp_op Some buf op1 op2)))"
+| "compile_subgraph c (Seq buf sg1 sg2) = map_op projl projr (comp_op Some buf (compile_subgraph c sg1) (compile_subgraph c sg2))"
 
 lemma
-  "activate io c t_op c'' t_op' \<Longrightarrow>
-   (c', op) = (compile_subgraph c t_op) \<Longrightarrow>
-   (c'', op') =  (compile_subgraph c' t_op') \<Longrightarrow>
-   step io op op'"
-  apply (induct io c t_op c'' t_op' arbitrary: op op' pred: activate)
-  subgoal for l c c' opa l' uu_ op'a io
+  "activate io c sg c' sg' \<Longrightarrow>
+   step io (compile_subgraph c sg) (compile_subgraph c' sg')"
+  apply (induct io c sg c' sg'  pred: activate)
+  subgoal for io l c l' c'
     by simp
-  subgoal for p x c op1 c' op1' buf op2
-    apply (simp split: prod.splits)
+  subgoal for p x c sg1 c' sg1' buf sg2
+    apply simp
       apply (rule step_map_op)
      apply simp_all
     apply (rule step_Tau_comp_op_L)
        apply simp_all
+    oops
    
 
       find_theorems comp_op Tau Out
