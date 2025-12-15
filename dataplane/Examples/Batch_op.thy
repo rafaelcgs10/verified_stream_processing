d3af90354b1123fd90dd04790a4017a6563ca87a
rafaelcgs10 <rafaelcgs10@gmail.com>
2025-12-15 11:36:12 +0100

    WIP: batch_op spec

--- dataplane/Examples/Batch_op.thy
+++ dataplane/Examples/Batch_op.thy
@@ -10,8 +10,8 @@ term "show_list show_nat"
 
 abbreviation "show_myprod \<equiv> (\<lambda> s1 s2. show_prod s1 s2 o to_prod)"
 
-definition batch_op where
-  "batch_op ips ops comb os logic = notifier_op ips ops os 
+definition batch_op_logic where
+  "batch_op_logic ips ops comb os logic = notifier_op ips ops os 
    (\<lambda> os compl_caps.
     let comb_caps = comb compl_caps in
     if (\<forall> p. comb_caps p = []) then trace (STR ''No capabilities'') {||} else
@@ -24,14 +24,14 @@ definition batch_op where
     (drop_caps (produces os (map (\<lambda> (d, cap). (en2 os d, cap)) outs)) drops)) outs_drops)"
 
 
-definition batch_fun_op where
-  "batch_fun_op os f = batch_op {|(1 :: 1)|} {|(1 :: 1)|} id os
+definition batch_op where
+  "batch_op os f = batch_op_logic {|(1 :: 1)|} {|(1 :: 1)|} id os
    (\<lambda> compl_batches ts caps. {| (concat (map (\<lambda> t. map (\<lambda> x. (x, Cap t 1)) (f (compl_batches 1 t))) (ts 1)), map (\<lambda> t. Cap t 1) (caps 1)) |})"
 
 
 (*
 definition diff_op where
-  "diff_op os = batch_op {|(1 :: 2), 2|} {|1|} 
+  "diff_op os = batch_op_logic {|(1 :: 2), 2|} {|1|} 
    (\<lambda> compl_caps p. if p = 1 then filter (\<lambda> t. t \<in> set (compl_caps 2)) (compl_caps 1) else filter (\<lambda> t. t \<in> set (compl_caps 1)) (compl_caps 2))
    (os\<lparr> de1 := projl, en2 := Inr \<rparr>)
    (\<lambda> compl_batches ts caps. 
--- dataplane/Examples/Batch_op_Correctness.thy
+++ dataplane/Examples/Batch_op_Correctness.thy
@@ -11,6 +11,8 @@ imports
   Set_op
 begin
 
+section \<open>Example\<close>
+
 abbreviation "t0 \<equiv> MyPair (0 :: nat) (0 :: nat)"
 abbreviation "t_1_0 \<equiv> MyPair (Suc 0) (0 :: nat)"
 abbreviation "t_0_1 \<equiv> MyPair (0 :: nat) (Suc 0)"
@@ -55,92 +57,99 @@ abbreviation init_operator_state_ty2 where
    en2 = Inr,
    de2 = projr
    \<rparr>"
-abbreviation "l2 f \<equiv> Logic (batch_fun_op (init_operator_state_ty2 default_internal_summary) f) default_internal_summary"
+abbreviation "l2 f \<equiv> Logic (batch_op (init_operator_state_ty2 default_internal_summary) f) default_internal_summary"
 
 abbreviation "test_dt2 \<equiv> Comp [(0, 1) \<mapsto> (0, 1)] (l1 (\<lambda> _. inps2)) (l2 (\<lambda> b. if b = [] then trace (STR ''Empty batch! ! !'') [] else [Max (set b)]))"
 
 abbreviation "test_op2 \<equiv> compile_dataflow test_dt2 :: (2 \<times> 1, 2 \<times> 1, _) op"
 
-
 value [GHC] "lmap (\<lambda> io. case io of VOut p (x, t) \<Rightarrow> (projr x, t)) (trace_exec test_op2)"
-
-find_theorems cUn name: code
-
-value "frontier {# t_1_1, t_0_1, t_1_0 #}\<^sub>z"
-
-term DEBUG
-
-find_theorems Max fold
-
-term test_op2
-
-abbreviation "timestamps inps \<equiv> cset_of_llist (lmap (\<lambda> ev. case ev of Data t d \<Rightarrow> t) (lfilter is_Data inps))"
-
-abbreviation "Max_spec inps \<equiv> 
-  cimage (\<lambda> t. (1 :: 1, Max (set (list_of (lmap (\<lambda> ev. case ev of Data _ d \<Rightarrow> d) (lfilter (\<lambda> ev. case ev of Data t' d \<Rightarrow> t = t' | _ \<Rightarrow> False) inps)))), t))
-  (timestamps inps)"
-
 value [GHC] "check_prefix 100 [((1, 1), (Inr 10, MyPair 1 1)), ((1, 1), (Inr 7, MyPair 0 1)),((1, 1), (Inr 3, MyPair 1 0))] test_op2"
 value [GHC] "check_prefix 100 [((1, 1), (Inr 7, MyPair 0 1)), ((1, 1), (Inr 10, MyPair 1 1)), ((1, 1), (Inr 3, MyPair 1 0))] test_op2"
 value [GHC] "check_prefix 100 [((1, 1), (Inr 3, MyPair 1 0)), ((1, 1), (Inr 10, MyPair 1 1)), ((1, 1), (Inr 7, MyPair 0 1))] test_op2"
 
-
-
-
-abbreviation "inp_op os \<equiv> map_op (case_option (Inl (0 :: 2)) (\<lambda> p. Inr (0, p))) (case_option (Inl (0 :: 2)) (\<lambda> p. Inr (0, p))) (ooo_input_op {|1|} os)"
-abbreviation "tt_op os f \<equiv> map_op (case_option (Inl (1 :: 2)) (\<lambda> p. Inr (1, p))) (case_option (Inl (1 :: 2)) (\<lambda> p. Inr (1, p))) (batch_fun_op os f)"
-
-abbreviation "inp_tt_op os1 cbuf os2 f \<equiv>
-   map_op (case_sum id id) (case_sum id id)
-   (comp_op [Inr (0 :: 2, 0 :: 1) \<mapsto> Inr (1 :: 2, 0 :: 1)] cbuf (inp_op (os1\<lparr> en1 := Inl \<rparr>)) (tt_op (os2\<lparr> de1 := projl, en2 := Inr \<rparr>) f))"
-
-
-definition \<open>subgraph_inv dtt cgs c = (let (su, _) = compile_dataflow_tree dtt in
- \<lparr> pt_tr = change_multiplicities su cgs c,
-   edges = (\<lambda> l1. [l2 \<leftarrow> Enum.enum. \<not> is_empty_antichain (su l1 l2) \<and> is_Src (port l1) \<and> is_Trg (port l2) ]),
-   summ = su,
-   upfro = undefined \<rparr>)\<close>
-
-lemma dataflow_op_inp_tt_op_wbisim_source_op_aux:
-  fixes lxs :: \<open>('t :: {ccompare,canonically_ordered_monoid_add,ordered_ab_semigroup_monoid_add_imp_le,bot}, 'd1) event llist\<close>
-  and f :: \<open>'d1 buf \<Rightarrow> 'd2 buf\<close>
-  and os1 :: \<open>(1, 'd1 + 'd2, 'd1, 't) input_state\<close>
-  and os2 :: \<open>(1, 'd1 + 'd2, 'd1, 'd2, 't) operator_state_ty2\<close>
-assumes
-  buffers_inv: 
-  \<open>es os1 1 = lxs\<close>
-  \<open>outpu os1 1 = map (\<lambda> (d, t). (Inl d, t)) out_os1\<close>
-  \<open>input os2 1 = map (\<lambda> (d, t). (Inl d, t)) inp_os2\<close>
-  \<open>buf = out_os1 @ cbuf @ inp_os2\<close>
-  and
-  subgraph_inv:
-  \<open>(a, st1) = obtain_progress os1\<close>   
-  \<open>(b, st2) = obtain_progress os2\<close>
-  \<open>cgs = extract_progress 0 (edges sg) st1 @ extract_progress 1 (edges sg) st2\<close>
-  \<open>sg = subgraph_inv test_dt1 cgs c\<close>
-  \<open>c' = pt_tr sg\<close>
-  and
-  c_pts_inv:
-  \<open>c_pts c' (Loc 0 (Trg 1)) = {#}\<^sub>z\<close>
-  \<open>c_pts c' (Loc 0 (Src 1)) = zmset_of (mset (ocaps os1 1))\<close>
-  \<open>c_pts c' (Loc 1 (Trg 0)) = zmset_of (mset (map snd buf))\<close>
-  \<open>c_pts c' (Loc 1 (Src 1)) = zmset_of (mset (ocaps os2 1))\<close>
-  and
-  c_imp_inv:
-  \<open>front os2 1 \<le> frontier (c_imp c (Loc 1 (Trg 0)))\<close>
-
-shows 
-  \<open>dataflow_op sg (inp_tt_op os1 (\<lambda> p. case p of Inl x \<Rightarrow> [] | Inr x \<Rightarrow> map (\<lambda> (d, t). Inr (Inl d, t)) cbuf) os2 f) \<approx>
-   map_op (\<lambda> p. (1, p)) (\<lambda> p. (1, p)) (source_op (\<lambda> p. outpu os2 1 @@- lmap (\<lambda> (d, t). (Inr d, t)) (lconcat (batch_fun_spec f lxs buf caps))))\<close>
-
-  oops
-
-
-abbreviation "G inps f \<equiv> (compile_dataflow (Comp [(0, 1) \<mapsto> (0, 1)] (l1 inps) (l2 f)))"
+section \<open>Generalized Correctness\<close>
 
 abbreviation "coll inps t \<equiv> list_of (lmap (\<lambda> e. case e of Data t d \<Rightarrow> d) (lfilter (\<lambda> e. case e of Data t' d \<Rightarrow> t = t' | _ \<Rightarrow> False) inps))"
 
 abbreviation "ts inps \<equiv> cimage (\<lambda> e. case e of Data t d \<Rightarrow> t) (cfilter is_Data (cset_of_llist inps))"
+
+abbreviation "inp_op os \<equiv> map_op (case_option (Inl (0 :: 2)) (\<lambda> p. Inr (0, p))) (case_option (Inl (0 :: 2)) (\<lambda> p. Inr (0, p))) (ooo_input_op {|1|} os)"
+abbreviation "tt_op os f \<equiv> map_op (case_option (Inl (1 :: 2)) (\<lambda> p. Inr (1, p))) (case_option (Inl (1 :: 2)) (\<lambda> p. Inr (1, p))) (batch_op os f)"
+
+abbreviation "G_op os1 cbuf os2 f \<equiv>
+   map_op (case_sum id id) (case_sum id id)
+   (comp_op [Inr (0 :: 2, 0 :: 1) \<mapsto> Inr (1 :: 2, 0 :: 1)] cbuf (inp_op (os1\<lparr> en1 := Inl \<rparr>)) (tt_op (os2\<lparr> de1 := projl, en2 := Inr \<rparr>) f))"
+
+definition "c_pts_inv c caps = (\<forall> l. c_pts c l = caps l)"
+definition "Src_caps_inv caps oss = (\<forall> nid p. caps (Loc nid (Src p)) = zmset_of (mset (ocaps (oss nid) p)))"
+definition "Trg_caps_inv caps bufs = (\<forall> nid p. caps (Loc nid (Trg p)) = zmset_of (mset (map snd (bufs nid p))))"
+definition "progress_inv eds oss = concat (map (\<lambda> nid. extract_progress nid eds (snd (obtain_progress (oss nid)))) Enum.enum)"
+definition "cgs_inv su cgs c = change_multiplicities su cgs c"
+definition "front_inv oss c = (\<forall> nid p. front (oss nid) 1 \<le> frontier (c_imp c (Loc nid (Trg p))))"
+definition "imp_front_inv su c = (\<forall> l. frontier (c_imp c l) \<le> dataflow_topology.implied_frontier_alt su (+) c l)"
+definition "buf_imp_front_inv su c T nid p = (\<forall> t \<in> T. frontier_less_equal (dataflow_topology.implied_frontier_alt su (+) c (Loc nid (Trg p))) t)"
+definition "changes_above_impl_inv su c cgs = (\<forall>(l, t, d)\<in>set cgs. frontier_less_equal (dataflow_topology.implied_frontier_alt su (+) c l) t)"
+definition "changes_non_zero_inv cgs = (\<forall>d\<in>snd ` snd ` set cgs. d \<noteq> 0)"
+definition "propagation_inv su c = 
+  (dataflow_topology.inv_imps_work_sum su (-+-) c \<and>
+   dataflow_topology.inv_implications_nonneg c \<and>
+   dataflow_topology.inv_imp_plus_work_nonneg c)"
+
+
+lemma correctness_gen:
+  fixes inps :: \<open>1 \<Rightarrow> ('t :: {ccompare,canonically_ordered_monoid_add,ordered_ab_semigroup_monoid_add_imp_le,bot}, 'd1) event llist\<close>
+    and f :: \<open>'d1 buf \<Rightarrow> 'd2 buf\<close>
+    and oss :: \<open>2 \<Rightarrow> (1, 'd1 + 'd2, 't) operator_state\<close>
+    and ip_state :: \<open>(1, 'd1 + 'd2, 'd1, 't) input_state\<close>
+    and bt_state :: \<open>(1, 'd1 + 'd2, 'd1, 'd2, 't) operator_state_ty2\<close>
+  assumes
+    SUBGRAPH_INV:
+    \<open>c = pt_tr sg\<close>
+    \<open>my_summ = summ sg\<close>
+    \<open>edges sg = (\<lambda> l. if l = Loc 0 (Src 1) then [Loc 1 (Trg 1)] else [])\<close>
+    and
+    OP_STATE_INV: 
+    \<open>ip_state = operator_state.extend (oss 1) \<lparr>en1 = Inl, de1 = projl, es = inps\<rparr>\<close>
+    \<open>bt_state = operator_state.extend (oss 2) \<lparr>en1 = Inl, de1 = projl, en2 = Inr, de2 = projr\<rparr>\<close>
+    and
+    BUFS_INV: 
+    \<open>outpu (oss 1) 1 = map (\<lambda> (d, t). (Inl d, t)) out_os1\<close>
+    \<open>input (oss 2) 1 = map (\<lambda> (d, t). (Inl d, t)) inp_os2\<close>
+    \<open>buf = out_os1 @ cbuf @ inp_os2\<close>
+    \<open>bufs = (\<lambda> nid p. if nid = 1 \<and> p = 0 then buf else [])\<close>
+    and
+    C_PTS_INV:
+    \<open>Src_caps_inv caps oss\<close>
+    \<open>Trg_caps_inv caps bufs\<close>
+    \<open>cgs = progress_inv (edges sg) oss\<close>
+    \<open>c' = cgs_inv my_summ cgs c\<close>
+    \<open>c_pts_inv c' caps\<close>
+    \<open>buf_imp_front_inv my_summ c (snd ` set buf) 1 0\<close>
+    and
+    C_IMP_INV:
+    \<open>front_inv oss c\<close>
+    \<open>imp_front_inv my_summ c\<close>
+    and
+    CGS_INV:
+    \<open>changes_above_impl_inv my_summ c cgs\<close>
+    \<open>changes_non_zero_inv cgs\<close>
+    and
+    PROP_INV:
+    \<open>propagation_inv my_summ c\<close>
+
+  shows 
+    \<open>set_op S D
+    (dataflow_op sg (G_op ip_state (\<lambda> p. case p of Inl x \<Rightarrow> [] | Inr x \<Rightarrow> map (\<lambda> (d, t). Inr (Inl d, t)) cbuf) bt_state f)) \<approx>
+    set_spec_op (cUn S S') D\<close>
+  oops
+
+
+
+
+section \<open>Correctness\<close>
+
+abbreviation "G inps f \<equiv> compile_dataflow (Comp [(0, 1) \<mapsto> (0, 1)] (l1 inps) (l2 f))"
 
 lemma
   fixes inps :: \<open>1 \<Rightarrow> ('t :: {ccompare,canonically_ordered_monoid_add,ordered_ab_semigroup_monoid_add_imp_le,bot}, 'd1) event llist\<close>
@@ -148,8 +157,7 @@ lemma
    and S :: \<open>((2 \<times> 1) \<times> ('d1 + 'd2) \<times> 't) cset\<close>
  assumes \<open>S = cUnion (cimage (\<lambda> t. (cset_of_llist o llist_of) (map (\<lambda> x. ((2, 1), (Inr x, t))) (f (coll (inps 1) t)))) (ts (inps 1)))\<close>
   shows \<open>set_op {||} {||} (G inps f) \<approx> set_spec_op S {||}\<close>
-
-
+  oops
 
 
 end
