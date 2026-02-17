320f81bd5926e6c431a0c8f2d72a51b4ec247820
rafaelcgs10 <rafaelcgs10@gmail.com>
2026-02-17 16:01:07 +0100

    More

--- dataplane/Correctness/Consumes.thy
+++ dataplane/Correctness/Consumes.thy
@@ -220,7 +220,7 @@ lemma dataplane_tracker_inv_consumes:
    dataflow_topology (summ sg) (-+-) \<Longrightarrow>
    graph_summar_nt (summ sg) (nxt sg) os \<Longrightarrow>
   dataplane_tracker_inv (os(nid := consumes (os nid) p (t :: 't :: {ccompare,canonically_ordered_monoid_add,ordered_ab_semigroup_monoid_add_imp_le,bot}) d)) (BTL (nid, p) cbufs) sg"
-  (* unfolding dataplane_tracker_inv_def
+  unfolding dataplane_tracker_inv_def
   apply (elim conjE exE)
   apply simp
   apply hypsubst_thin
@@ -4984,7 +4984,6 @@ lemma dataplane_tracker_inv_consumes:
       apply auto
       done
     done
-  done*)
-  sorry
+  done
 
 end
